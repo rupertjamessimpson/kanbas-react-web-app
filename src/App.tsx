6a0076ce90ac9fad47b55696a3068cdf39c73539
rupertjamessimpson <87886963+rupertjamessimpson@users.noreply.github.com>
2024-01-23 00:28:46 -0500

    deplopying....

--- src/App.tsx
+++ src/App.tsx
@@ -7,17 +7,9 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <a href="/Labs/a1/index.html">Lab 1</a>
+        <br />
+        <a href="/Kanbas/Courses/Home/screen.html">A1</a>
       </header>
     </div>
   );
